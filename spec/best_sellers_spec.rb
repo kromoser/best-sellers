7aa323231d598654162caca7c38a80bc9ff94e66
kromoser <kevinromoser@gmail.com>
2016-05-18 00:06:33 -0400

    First commit - Set up project structure with bundler

---
+++ spec/best_sellers_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe BestSellers do
+  it 'has a version number' do
+    expect(BestSellers::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
